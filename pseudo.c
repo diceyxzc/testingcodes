2cc5ec0a9328d4f3f5c0077d95a331a1b6209344
Calvin Berlandino <2023-2-02178@lpunetwork.edu.ph>
2024-09-27 15:36:22 +0800

    mm

---
+++ pseudo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+      srand(time (0));
+
+      int number1 = rand() % 6 + 1;
+
+      printf("%d", number1);
+
+    return 0;
+}
