c1cee5b0068e4162a38a2696c99d25069c83518e
Calvin Berlandino <2023-2-02178@lpunetwork.edu.ph>
2024-09-29 08:52:11 +0800

    aa

---
+++ twodecimalplaces.py
@@ -0,0 +1,8 @@
+#two decimal places
+
+length = float(input("Enter length of a rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+
+print(f"{area:.2f} is the equivalent area of the inputted measurements.")
